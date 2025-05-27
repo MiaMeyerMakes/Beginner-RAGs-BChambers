466d4acc6602197998ce44302c35cc73981532d0
MiaMeyerMakes <miameyer15@gmail.com>
2025-05-27 11:40:08 +0200

    Basic implementation

--- RAG_beginners_project/RAG_implementation.py
+++ RAG_beginners_project/RAG_implementation.py
@@ -10,3 +10,21 @@ corpus_of_documents = [
     "Attend a workshop or lecture on a topic you're interested in.",
     "Visit an amusement park and ride the roller coasters."
 ]
+def jaccard_similarity(query, document):
+    query = query.lower().split(" ")
+    document = document.lower().split(" ")
+    intersection = set(query).intersection(set(document))
+    union = set(query).union(set(document))
+    return len(intersection)/len(union)
+
+def return_response(query, corpus):
+    similarities = []
+    for doc in corpus:
+        similarity = jaccard_similarity(user_input, doc)
+        similarities.append(similarity)
+    return corpus_of_documents[similarities.index(max(similarities))]
+
+user_prompt = "What is a leisure activity that you like?"
+user_input = "I like to hike"
+
+print(return_response(user_input, corpus_of_documents))
